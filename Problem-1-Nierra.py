613a9932a030867af12c77d92cd5275d1334bdaf
Chrizza Nierra <nierrachrizza@gmail.com>
2026-07-09 11:38:02 +0800

    Add files via upload

---
+++ Problem-1-Nierra.py
@@ -0,0 +1,12 @@
+name = str(input("What is your name?"))
+age = int(input("How old are you?"))
+subject = str(input("What is your favorite subject?"))
+
+
+print(f"""
+==== My Info Card =====
+Name    : {name}
+Age     : {age}
+Subject : {subject}
+=======================
+""")
