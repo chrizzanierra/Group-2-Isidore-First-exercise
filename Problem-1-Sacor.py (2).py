9d7cb1d0e3b3ca2402472561737500072a14dd3f
azariastelle070415-netizen <azariastelle070415@gmail.com>
2026-07-09 11:26:57 +0800

    Add files via upload

---
+++ Problem-1-Sacor.py (2).py
@@ -0,0 +1,22 @@
+
+
+name = "Azaria"
+age = 15
+gpa = 3.8
+is_enrolled = True
+
+
+print(name)
+print(type(name))
+
+
+print(age)
+print(type(age))
+
+
+print(gpa)
+print(type(gpa))
+
+
+print(is_enrolled)
+print(type(is_enrolled))
